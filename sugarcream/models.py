41a181a472902f46859dae5b6880d0298dfc3208
Freddie Park <sorelove@gmail.com>
2012-01-07 19:28:53 +0900

    added Project model and testcase for that.

--- sugarcream/models.py
+++ sugarcream/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+# -*- condig: utf-8 -*-
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Project(models.Model):
+    owner = models.ForeignKey(User, related_name='owned_project_set')
+    name = models.CharField(max_length=128, unique=True)
+    collaborators = models.ManyToManyField(User, related_name='project_set')
--- sugarcream/tests.py
+++ sugarcream/tests.py
@@ -1,3 +1,5 @@
+# -*- condig: utf-8 -*-
+
 """
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
@@ -6,11 +8,63 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from sugarcream.models import Project
+from django.contrib.auth.models import User
 
+class ProjectTest(TestCase):
+    def setUp(self):
+        testOwner = User.objects.create_user(username='testOwner',
+                                             password='password',
+                                             email='testOwner@sugarcream.org')
+        project1 = Project(owner=testOwner, name='project1')
+        project1.save()
+        project2 = Project(owner=testOwner, name='project2')
+        project2.save()
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def testNameUniqueness(self):
+        testOwner = User.objects.get(username='testOwner')
+        try:
+            project = Project(owner=testOwner, name='testProject')
+            project.save()
+            self.fail()
+        except:
+            pass
+
+    def testOwner(self):
+        project = Project.objects.get(id=1)
+        self.assertEqual(project.owner.username, 'testOwner')
+        owner = User.objects.get(username='testOwner')
+        self.assertEqual(owner.owned_project_set.count(), 2)
+
+    def testName(self):
+        project = Project.objects.get(id=1)
+        self.assertEqual(project.name, 'project1')
+
+    def testCollaborators(self):
+        user1 = User.objects.create_user(username='user1',
+                                         password='password',
+                                         email='user1@sugarcream.org')
+        user2 = User.objects.create_user(username='user2',
+                                         password='password',
+                                         email='user2@sugarcream.org')
+        user3 = User.objects.create_user(username='user3',
+                                         password='password',
+                                         email='user3@sugarcream.org')
+        project1 = Project.objects.get(name='project1')
+        project2 = Project.objects.get(name='project2')
+        project1.collaborators.add(user1)
+        project1.collaborators.add(user2)
+        project2.collaborators.add(user2)
+        project2.collaborators.add(user3)
+
+        self.assertEqual(user1.project_set.count(), 1)
+        self.assertEqual(user2.project_set.count(), 2)
+        self.assertEqual(user3.project_set.count(), 1)
+
+    def tearDown(self):
+        projects = Project.objects.all()
+        for project in projects:
+            project.delete()
+        users = User.objects.all()
+        for user in users:
+            user.delete()
