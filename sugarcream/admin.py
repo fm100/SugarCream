8afbd0f6595adf638ef809d56429701d4c0d633a
Freddie Park <sorelove@gmail.com>
2012-01-28 01:23:07 +0900

    added admin page.

---
+++ sugarcream/admin.py
@@ -0,0 +1,6 @@
+# -*- condig: utf-8 -*-
+
+from sugarcream.models import Project, BacklogItem
+from django.contrib import admin
+
+admin.site.register(Project)
--- urls.py
+++ urls.py
@@ -2,9 +2,8 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib.auth.views import login as loginpage
 from sugarcream.views import *
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -15,7 +14,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', mainpage),
     url(r'^login/$', loginpage),
